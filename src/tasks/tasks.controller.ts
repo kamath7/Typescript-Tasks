1d1a80542f52ce13cc76dd63aa184f9c5a5250ec
Adithya Kamath <adithyakamath96@gmail.com>
2020-11-04 02:54:15 +0530

    Added POST, DELETE routes

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
 import { Task } from './task.model';
 import { TasksService } from './tasks.service';
 
@@ -9,5 +10,23 @@ export class TasksController {
   @Get()
   getAllTasks(): Task[] {
     return this.tasksService.getAllTasks();
-  } 
+  }
+
+  @Get('/:id')
+  getTaskById(@Param('id') id:string):Task{
+    return this.tasksService.getTaskById(id)
+  }
+
+  @Post()
+  createTask(
+   @Body() createTaskDto: CreateTaskDto
+  ) : Task {
+    return this.tasksService.createTask(createTaskDto);
+  }
+
+  @Delete('/:id')
+  deleteTask(@Param('id') id:string):void{
+     this.tasksService.deleteTask(id);
+  }
+
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { Task } from './task.model';
-
+import { Task, TaskStatus } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
+import * as uuid from 'uuid';
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
@@ -8,4 +9,30 @@ export class TasksService {
   getAllTasks(): Task[] {
     return this.tasks;
   }
+
+  getTaskById(id: string): Task {
+    return this.tasks.find(task => task.id === id);
+  }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
+    const task: Task = {
+      id: uuid.v1(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+    this.tasks.push(task);
+    return task;
+  }
+
+  deleteTask(id: string) {
+     this.tasks = this.tasks.filter(task => task.id !== id);
+  }
 }
+//DTO - How data is sent over the network
+
+/*
+
+Classes to be used for DTOs rather than interface
+*/
