d759c68d0b5bf0e1e3cebc1b57fc19f0227295fc
Adithya Kamath <adithyakamath96@gmail.com>
2020-11-18 20:40:35 +0530

    Added relationship in DB

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,5 +1,12 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
-import {TaskStatus} from './tasks.status.enum';
+import { User } from 'src/auth/user.entity';
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { TaskStatus } from './tasks.status.enum';
 //Database Schema
 @Entity()
 export class Task extends BaseEntity {
@@ -14,4 +21,13 @@ export class Task extends BaseEntity {
 
   @Column()
   status: TaskStatus;
+  @ManyToOne(
+    type => User,
+    user => user.tasks,
+    { eager: false },
+  )
+  user: User;
+
+  @Column()
+  userId: number;
 }
--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -1,3 +1,4 @@
+import { User } from "src/auth/user.entity";
 import { Entity, EntityRepository, Repository } from "typeorm";
 import {TaskStatus} from '../tasks/tasks.status.enum';
 import { CreateTaskDto } from "./dto/create-task.dto";
@@ -7,7 +8,7 @@ import { Task } from "./task.entity";
 @EntityRepository(Task)
 export class TaskRepository extends Repository<Task>{
 
-async getTasks (filterDto: GetTasksFilterDto): Promise<Task[]>{
+async getTasks (filterDto: GetTasksFilterDto, user:User): Promise<Task[]>{
     const {status, search} = filterDto;
     const query = this.createQueryBuilder('task')
 
@@ -23,14 +24,17 @@ async getTasks (filterDto: GetTasksFilterDto): Promise<Task[]>{
 }
 
 
-async createTask(createTaskDto: CreateTaskDto ): Promise<Task>{
+async createTask(createTaskDto: CreateTaskDto, user: User ): Promise<Task>{
     const task = new Task();
     const { title, description } = createTaskDto;
 
     task.title = title;
     task.description = description;
+    task.user = user;
     task.status = TaskStatus.OPEN;
     await task.save();
+    delete task.user;
+
     return task;
 }
 }
